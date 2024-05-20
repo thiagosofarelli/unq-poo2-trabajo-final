9e2714f5fac2495c3410105e6dcc9ac2763f94d5
Thiago <sofarelli.thiago10@gmail.com>
2024-05-20 16:25:31 -0300

    Registro de Compra finalizado. Ahora el punto de venta realiza los registros por app y por compra puntual, y el SEM los instancia y aniade a su lista de Registros de Compra.

--- tp-final/src/sem/PuntoDeVenta.java
+++ tp-final/src/sem/PuntoDeVenta.java
@@ -5,19 +5,19 @@ import app.App;
 public class PuntoDeVenta {
 
 	private SistemaEstacionamientoMedido sem;
-	private int nroRegistro;
+	private int nroControl;
 	
 	public PuntoDeVenta(SistemaEstacionamientoMedido sem) {
 		this.sem = sem;
-		this.nroRegistro = 0;
+		this.nroControl = 0;
 	}
 	
 	public void registrarCargaDeCredito(App app, float monto) {
-		this.sem.registrarCargaDeCredito(this, app, monto, nroRegistro);
-		this.nroRegistro++;
+		this.sem.registrarCargaDeCredito(this, app, monto, nroControl);
+		this.nroControl++;
 	}
 
 	public void registrarCompraPuntual(String patente, int cantidadHoras) {
-		this.sem.registrarCompraPuntual(this, patente, cantidadHoras, nroRegistro);
+		this.sem.registrarCompraPuntual(this, patente, cantidadHoras, nroControl);
 	}
 }
