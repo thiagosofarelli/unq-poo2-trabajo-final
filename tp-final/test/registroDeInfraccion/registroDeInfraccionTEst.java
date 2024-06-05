3edd66401d1e5d64814296fc485f3dcf1eec7260
Lara Noelí Cardozo <cardozo.lara.edu@gmail.com>
2024-06-05 19:00:36 -0300

    creacion test

---
+++ tp-final/test/registroDeInfraccion/registroDeInfraccionTEst.java
@@ -0,0 +1,19 @@
+package registroDeInfraccion;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class registroDeInfraccionTEst {
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
